313b7a0a50c83cd915a9130913ca3b1da46d0764
Shubhdeep Singh <149824672+iamshubhdeep@users.noreply.github.com>
2025-02-16 12:48:37 +0530

    Update SceneManager.js

--- js/SceneManager.js
+++ js/SceneManager.js
@@ -1,50 +1,40 @@
-/*
-The SceneManager is responsible for handling the Three.js side of the app, 
-which is completely hidden from the main. It knows nothing about the DOM.
+import * as THREE from 'three';
+import { MyCameraControls } from '../cameraControls/MyCameraControls.js';
+import { PickHelper } from '../PickHelper.js';
+import { SolarSystem } from '../sceneSubjects/SolarSystem.js';
+import { AmbientLight } from '../sceneSubjects/AmbientLight.js';
+import { SunLight } from '../sceneSubjects/SunLight.js';
+import { Stars } from '../sceneSubjects/astronomicalBodies/Stars.js';
 
+export class SceneManager {
+    constructor(canvas) {
+        this.screenDimensions = {
+            width: canvas.width,
+            height: canvas.height
+        };
+        this.scene = this.buildScene();
+        this.renderer = this.buildRender(this.screenDimensions);
+        this.camera = this.buildCamera(this.screenDimensions);
+        this.sceneSubjects = this.createSceneSubjects(this.scene);
+        this.cameraControls = new MyCameraControls(this.camera, canvas);
 
-SceneManager is the component responsible for managing the scene. 
-It works at high level, it shouldn’t know any detail about the content of the scene.
-His responsibilities are:
-    * create Scene, Renderer and Camera.
-    * initialize a bunch of SceneSubjects.
-    * update everything at every frame.
+        this.scene.background = new THREE.Color('black');
 
+        // Picker
+        this.pickPosition = { x: 0, y: 0 };
+        this.pickHelper = new PickHelper();
+        this.clearPickPosition();
 
-A SceneSubject represents one entity in the scene. 
-The SceneManager usually contains multiple SceneSubjects.
-*/
-
-
-function SceneManager(canvas) {
-
-    // scene setup
-    const screenDimensions = {
-        width: canvas.width,
-        height: canvas.height
+        this.setupEventListeners(canvas);
     }
-    const scene = buildScene();
-    const renderer = buildRender(screenDimensions);
-    const camera = buildCamera(screenDimensions);
-    const sceneSubjects = createSceneSubjects(scene);
 
-    const cameraControls = new MyCameraControls(camera, canvas);
-
-    scene.background = new THREE.Color('black');
-
-    //Picker
-    const pickPosition = { x: 0, y: 0 };
-    const pickHelper = new PickHelper();
-    clearPickPosition();
-
-    function buildScene() {
+    buildScene() {
         const scene = new THREE.Scene();
         scene.background = new THREE.Color("#000");
-
         return scene;
     }
 
-    function buildRender({ width, height }) {
+    buildRender({ width, height }) {
         const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
         const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
         renderer.setPixelRatio(DPR);
@@ -56,7 +46,7 @@ function SceneManager(canvas) {
         return renderer;
     }
 
-    function buildCamera({ width, height }) {
+    buildCamera({ width, height }) {
         const aspectRatio = width / height;
         const fieldOfView = 60;
         const nearPlane = 1;
@@ -69,7 +59,7 @@ function SceneManager(canvas) {
         return camera;
     }
 
-    function createSceneSubjects(scene) {
+    createSceneSubjects(scene) {
         const sceneSubjects = [
             new SolarSystem(scene),
             new AmbientLight(scene),
@@ -80,35 +70,30 @@ function SceneManager(canvas) {
         return sceneSubjects;
     }
 
-    // It is called by the main at every frame.
-    this.update = function (time) {
-        for (let i = 0; i < sceneSubjects.length; i++)
-            sceneSubjects[i].update(time);
+    update(time) {
+        for (let i = 0; i < this.sceneSubjects.length; i++) {
+            this.sceneSubjects[i].update(time);
+        }
 
-
-        let x = sceneSubjects[0];
-        pickHelper.pick(pickPosition, x.getAstrionomicalBodies(), camera, time, scene);
-        cameraControls.update();
-        renderer.render(scene, camera);
+        const solarSystem = this.sceneSubjects[0];
+        this.pickHelper.pick(this.pickPosition, solarSystem.getAstrionomicalBodies(), this.camera, time, this.scene);
+        this.cameraControls.update();
+        this.renderer.render(this.scene, this.camera);
     }
 
-    // Updates the aspect ratio of the camera and the size of the Renderer. 
-    // It is called by the main each time the window is resized.
-    this.onWindowResize = function () {
+    onWindowResize() {
         const { width, height } = canvas;
 
-        screenDimensions.width = width;
-        screenDimensions.height = height;
+        this.screenDimensions.width = width;
+        this.screenDimensions.height = height;
 
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
 
-        renderer.setSize(width, height);
+        this.renderer.setSize(width, height);
     }
 
-
-    // Picker
-    function getCanvasRelativePosition(event) {
+    getCanvasRelativePosition(event) {
         const rect = canvas.getBoundingClientRect();
         return {
             x: (event.clientX - rect.left) * canvas.width / rect.width,
@@ -116,69 +101,56 @@ function SceneManager(canvas) {
         };
     }
 
-    function setPickPosition(event) {
-        const pos = getCanvasRelativePosition(event);
-        pickPosition.x = (pos.x / canvas.width) * 2 - 1;
-        pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+    setPickPosition(event) {
+        const pos = this.getCanvasRelativePosition(event);
+        this.pickPosition.x = (pos.x / canvas.width) * 2 - 1;
+        this.pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
     }
 
-    function clearPickPosition() {
-        // unlike the mouse which always has a position
-        // if the user stops touching the screen we want
-        // to stop picking. For now we just pick a value
-        // unlikely to pick something
-        pickPosition.x = undefined;
-        pickPosition.y = undefined;
+    clearPickPosition() {
+        this.pickPosition.x = undefined;
+        this.pickPosition.y = undefined;
     }
 
-    window.addEventListener('dblclick', setPickPosition);
-    window.addEventListener('mouseout', clearPickPosition);
-    window.addEventListener('mouseleave', clearPickPosition);
+    setupEventListeners(canvas) {
+        window.addEventListener('dblclick', (event) => this.setPickPosition(event));
+        window.addEventListener('mouseout', () => this.clearPickPosition());
+        window.addEventListener('mouseleave', () => this.clearPickPosition());
 
-    // Mobile support
-    window.addEventListener('touchstart', (event) => {
-        // prevent the window from scrolling
-        event.preventDefault();
-        setPickPosition(event.touches[0]);
-    }, { passive: false });
+        // Mobile support
+        window.addEventListener('touchstart', (event) => {
+            event.preventDefault();
+            this.setPickPosition(event.touches[0]);
+        }, { passive: false });
 
-    window.addEventListener('touchmove', (event) => {
-        setPickPosition(event.touches[0]);
-    });
+        window.addEventListener('touchmove', (event) => {
+            this.setPickPosition(event.touches[0]);
+        });
 
-    window.addEventListener('touchend', clearPickPosition);
+        window.addEventListener('touchend', () => this.clearPickPosition());
 
-
-
-    {
         // Modal
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        var modal = document.getElementById("myModal");
+        const span = document.getElementsByClassName("close")[0];
+        const modal = document.getElementById("myModal");
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
+        span.onclick = () => {
             modal.style.display = "none";
-        }
+        };
 
-        span.addEventListener('touchend', (event) => {
+        span.addEventListener('touchend', () => {
             modal.style.display = "none";
         });
 
-
-        // When the user clicks anywhere outside of the modal, close it (mobile)
         window.addEventListener('touchend', (event) => {
-            if (event.target == modal) {
+            if (event.target === modal) {
                 modal.style.display = "none";
             }
         });
 
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
+        window.onclick = (event) => {
+            if (event.target === modal) {
                 modal.style.display = "none";
             }
-        }
+        };
     }
 }
